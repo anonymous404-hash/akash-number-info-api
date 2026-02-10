5610bb138d416c320f338b6c64b001129970fc7b
API-HACKERS <metrom6x1@gmail.com>
2026-02-10 11:18:28 +0530

    Update number.js

--- pages/api/number.js
+++ pages/api/number.js
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
         return res.status(400).json({ error: "number missing" });
     }
 
-    const url = `https://original.proportalxc.workers.dev/?num=${number}`;
+    const url = `https://snxrajput-trial-api.vercel.app/number/=${number}`;
 
     try {
         const response = await fetch(url);
