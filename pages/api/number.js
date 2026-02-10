bedbf3c87aa5ee4e35df27b67952ad581e132a9c
API-HACKERS <metrom6x1@gmail.com>
2026-02-10 20:27:20 +0530

    Update number.js

--- pages/api/number.js
+++ pages/api/number.js
@@ -4,7 +4,7 @@ const DEVELOPER = "@AKASHHACKER"; // Aapka handle
 const KEYS_DB = {
   "AKASH_PAID31DAYS": { expiry: "2026-03-31", status: "Premium" },
   "AKASH_PAID1DAYS": { expiry: "2026-02-15", status: "Basic" },
-  "ZEXX_VIP": { expiry: "2026-12-31", status: "Premium" },
+  "ZEXX_VIP": { expiry: "2026-12-31", status: "Premium" }
 };
 
 module.exports = async (req, res) => {
